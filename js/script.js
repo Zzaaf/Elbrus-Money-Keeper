3d30422bdec936a514c593e21ab561e2a19526ae
Yuriy <zzaafamania@gmail.com>
2024-05-08 23:20:15 +0300

    Expanded account logout functionality

--- js/script.js
+++ js/script.js
@@ -11,6 +11,8 @@ const incomeTable = document.querySelector('#incomeTable');
 const expensesTable = document.querySelector('#expensesTable');
 const toastLive = document.getElementById('liveToast');
 const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLive);
+const logoutBtn = document.querySelector('#logoutBtn');
+const changeForm = document.querySelector('#changeForm');
 
 if (authForm) {
     authForm.addEventListener('submit', (event) => {
@@ -22,7 +24,7 @@ if (authForm) {
         const userData = JSON.parse(localStorage.getItem('userData')) || {
             email: '',
             password: '',
-            balance: 0,
+            balance: null,
             income: [],
             expenses: []
         };
@@ -33,7 +35,7 @@ if (authForm) {
             localStorage.setItem('userData', JSON.stringify(userData));
             window.location.href = '/dashboard.html';
         } else {
-            if (userData.password !== password ||  userData.email !== email) {
+            if (userData.password !== password || userData.email !== email) {
                 toastBootstrap.show();
             } else {
                 window.location.href = '/dashboard.html';
@@ -47,14 +49,25 @@ if (profileName && JSON.parse(userDataStorage).email ) {
 }
 
 if (balance) {
-    const data = JSON.parse(localStorage.getItem('userData')) || { income: [], expenses: [], balance: 0 };
+    const data = JSON.parse(userDataStorage)                       
+    data.balance = data.income.reduce((total, income) => total + income.sum, 0) - data.expenses.reduce((total, expense) => total + expense.sum, 0);
+    localStorage.setItem('userData', JSON.stringify(data))
 
-    balance.value = data.balance;;
+    balance.value = data.balance;
+
+    if (data.balance) {
+        balance.setAttribute('disabled', true);
+        btnSetBalance.setAttribute('disabled', true);
+    }
 }
 
 if (btnSetBalance) {
     btnSetBalance.addEventListener('click', () => {
-        localStorage.setItem('userData', JSON.stringify({...JSON.parse(userDataStorage), balance: Number(balance.value)}))
+        const data = JSON.parse(userDataStorage);            
+        data.income.push({type: 'income', description: 'Пополнение', sum: Number(balance.value)});
+        data.balance = Number(balance.value);
+
+        localStorage.setItem('userData', JSON.stringify(data))
 
         balance.setAttribute('disabled', true);
         btnSetBalance.setAttribute('disabled', true);
@@ -72,8 +85,7 @@ if (typeTransaction) {
        if (typeTransaction.value === 'income' || typeTransaction.value === 'expenses') {
             const data = JSON.parse(userDataStorage);
             
-            data[typeTransaction.value].push(transaction);
-            
+            data[typeTransaction.value].push(transaction);            
             data.balance = data.income.reduce((total, income) => total + income.sum, 0) - data.expenses.reduce((total, expense) => total + expense.sum, 0);
 
             balance.value = data.balance;
@@ -138,5 +150,20 @@ if (expensesTable) {
         tr.appendChild(tdSum)
 
         expensesTable.appendChild(tr)
+    })
+}
+
+if (logoutBtn) {
+    logoutBtn.addEventListener('click', () => window.location.href = '/auth.html')
+}
+
+if (changeForm) {
+    const userData = JSON.parse(localStorage.getItem('userData'))
+    changeForm.email.value = userData.email;
+    changeForm.password.value = userData.password;
+
+    changeForm.addEventListener('submit', () => {
+        localStorage.setItem('userData', JSON.stringify({...userData, email: changeForm.email.value, password: changeForm.password.value }));
+        window.location.href = '/dashboard.html';
     })
 }
